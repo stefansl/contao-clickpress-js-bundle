c41cda64f25930cc75026ed9190fbf0568c3c468
stefansl <ssl@clickpress.de>
2025-10-29 18:57:41 +0100

    Adjust indentation in search.js for improved code readability

--- public/search.js
+++ public/search.js
@@ -7,52 +7,52 @@
 
 (function () {
 
-  'use strict';
+    'use strict';
 
-  const btnSearch = document.querySelectorAll('a.btn-search'),
-    searchBar = document.getElementById('searchbar'),
-    inputSearch = searchBar.querySelector('input.text'),
-    btncloseSearch = document.getElementById('close-search');
+    const btnSearch = document.querySelectorAll('a.btn-search'),
+        searchBar = document.getElementById('searchbar'),
+        inputSearch = searchBar.querySelector('input.text'),
+        btncloseSearch = document.getElementById('close-search');
 
-  // close search
-  const closeSearch = function (event) {
+    // close search
+    const closeSearch = function (event) {
 
-    if (event) {
-      event.preventDefault();
+        if (event) {
+            event.preventDefault();
+        }
+
+        if (searchBar) {
+            searchBar.classList.remove('is-active');
+        }
+
+        document.body.classList.remove('stop-scrolling');
+        document.body.classList.remove('search-open');
+    };
+
+    // Open search
+    const openSearch = function (event) {
+
+        if (event) {
+            event.preventDefault();
+        }
+        if (searchBar) {
+            searchBar.classList.toggle('is-active');
+        }
+        document.body.classList.toggle('stop-scrolling');
+        document.body.classList.toggle('search-open');
+        inputSearch.focus();
+
+        window.addEventListener('keyup', function (event) {
+            if (event.key === 'Escape') {
+                closeSearch();
+            }
+        });
+    };
+
+    // Add listeners
+    for (let i = 0; i < btnSearch.length; i++) {
+        btnSearch[i].addEventListener('click', openSearch);
     }
 
-    if (searchBar) {
-      searchBar.classList.remove('is-active');
-    }
-
-    document.body.classList.remove('stop-scrolling');
-    document.body.classList.remove('search-open');
-  };
-
-  // Open search
-  const openSearch = function (event) {
-
-    if (event) {
-      event.preventDefault();
-    }
-    if (searchBar) {
-      searchBar.classList.toggle('is-active');
-    }
-    document.body.classList.toggle('stop-scrolling');
-    document.body.classList.toggle('search-open');
-    inputSearch.focus();
-
-    window.addEventListener('keyup', function (event) {
-      if (event.key === 'Escape') {
-        closeSearch();
-      }
-    });
-  };
-
-  // Add listeners
-  for (var i = 0; i < btnSearch.length; i++) {
-    btnSearch[i].addEventListener('click', openSearch);
-  }
-
-  btncloseSearch.addEventListener('click', closeSearch);
+    btncloseSearch.addEventListener('click', closeSearch);
 })();
